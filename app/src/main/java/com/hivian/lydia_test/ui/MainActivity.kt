592e32be057104ff4b669f7b5ed42c57e0144460
Hugues Ivian <hivian@cegid.com>
2022-11-07 15:40:47 +0100

    Start migrating to compose

--- app/src/main/java/com/hivian/lydia_test/ui/MainActivity.kt
+++ app/src/main/java/com/hivian/lydia_test/ui/MainActivity.kt
@@ -1,21 +1,53 @@
 package com.hivian.lydia_test.ui
 
 import android.os.Bundle
-import com.hivian.lydia_test.R
-import com.hivian.lydia_test.core.base.ActivityBase
-import com.hivian.lydia_test.core.servicelocator.IoC
-import com.hivian.lydia_test.core.services.navigation.INavigationService
-import com.hivian.lydia_test.databinding.ActivityMainBinding
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.hivian.lydia_test.ui.composables.Detail
+import com.hivian.lydia_test.ui.composables.Home
+import com.hivian.lydia_test.ui.themes.ComposetestTheme
 
-class MainActivity : ActivityBase<ActivityMainBinding>(R.layout.activity_main) {
-
-    private val navigationService: INavigationService
-        get() = IoC.resolve()
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        navigationService.navigationActivity = this
-
         super.onCreate(savedInstanceState)
-    }
 
+        setContent {
+            ComposetestTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = NavRoutes.Home.route,
+    ) {
+        composable(NavRoutes.Home.route) {
+            Home(navController = navController)
+        }
+
+        composable(NavRoutes.Detail.route) {
+            Detail(navController = navController)
+        }
+    }
 }
