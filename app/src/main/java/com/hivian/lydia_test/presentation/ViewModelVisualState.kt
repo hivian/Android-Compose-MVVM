a4159d5fcae8ca235ad6da1cd85ae0e39ae46fb7
Hugues Ivian <hivian@cegid.com>
2022-11-12 23:38:48 +0100

    Update gradle

--- app/src/main/java/com/hivian/lydia_test/presentation/ViewModelVisualState.kt
+++ app/src/main/java/com/hivian/lydia_test/presentation/ViewModelVisualState.kt
@@ -3,13 +3,13 @@ package com.hivian.lydia_test.presentation
 import com.hivian.lydia_test.core.services.networking.ResourceErrorType
 
 sealed class ViewModelVisualState {
+
     object Loading: ViewModelVisualState()
+
     data class Error(val errorType: ResourceErrorType): ViewModelVisualState()
+
     object Success: ViewModelVisualState()
+
     object Unknown: ViewModelVisualState()
 
-    val isLoading : Boolean get() = this is Loading
-    val isError : Boolean get() = this is Error
-    val isSuccess : Boolean get() = this is Success
-    val isUnknown : Boolean get() = this is Unknown
 }
