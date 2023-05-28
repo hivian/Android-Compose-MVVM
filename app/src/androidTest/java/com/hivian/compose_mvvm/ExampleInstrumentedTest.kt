bbedc769b600e6169d89b12bb1a6bc5ff9755e2f
Hugues Ivian <hivian@cegid.com>
2023-05-29 00:38:01 +0200

    Build error fix

--- app/src/androidTest/java/com/hivian/compose_mvvm/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/hivian/compose_mvvm/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.hivian.mvvm_compose", appContext.packageName)
+        assertEquals("com.hivian.compose_mvvm", appContext.packageName)
     }
 }
